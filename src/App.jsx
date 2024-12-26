b0c237fcfc79a31384c2d8d693f94cf32e769caa
Saqib Arshad <saqibarshad429@gmail.com>
2024-12-26 13:32:00 +0500

    Upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
+import ProductsPage from "./pages/ProductsPage";
+import UsersPage from "./pages/UsersPage";
+import Sidebar from "./Components/Sidebar";
+function App() {
+  return (
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      <div className="fixed inset-0 z-0">
+        <div className="inset-0 absolute bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80 " />
+        <div className="absolute inset-0 backdrop-blur-sm " />
+      </div>
+
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+
+        <Route path="/users" element={<UsersPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Header from "../common/Header";
+import { motion } from "framer-motion";
+import StatCard from "../common/StatCard";
+import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
+import SalesOverviewChart from "../Components/overview/SalesOverviewChart";
+import CategoryDistributionChart from "../Components/overview/CategoryDistributionChart";
+import SalesChannelChart from "../Components/overview/SalesChannelChart";
+
+const OverviewPage = () => {
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Overview" />
+
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        {/* Stats */}
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCard
+            name="Total Sales"
+            icon={Zap}
+            value="Rs 1,234,567"
+            color="6366F1"
+          />
+          <StatCard name="New Users" icon={Users} value="123" color="8B5CF6" />
+          <StatCard
+            name="Total Products"
+            icon={ShoppingBag}
+            value="18"
+            color="EC4899"
+          />
+          <StatCard
+            name="Conversion Rates"
+            icon={BarChart2}
+            value="12.6%"
+            color="10B981"
+          />
+        </motion.div>
+
+        {/* Carts */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <SalesOverviewChart />
+          <CategoryDistributionChart />
+          <SalesChannelChart />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default OverviewPage;
